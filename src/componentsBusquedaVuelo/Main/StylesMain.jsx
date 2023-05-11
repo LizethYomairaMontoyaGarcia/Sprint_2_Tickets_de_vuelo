7c4b59c6dd08d516d1518d422000d50ef52d5023
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-05-11 13:54:55 -0500

    trabajando en el main con estilos

---
+++ src/componentsBusquedaVuelo/Main/StylesMain.jsx
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+
+export const PaymentMethods = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 24px 32px 20px;
+
+  @media screen and (max-width: 765px) {
+    flex-direction: column;
+    
+  }
+`;
+
+export const ImgPay = styled.figure`
+  img {
+    padding: 15px 10px;
+  }
+`;
+
+export const ContainerMain = styled.div`
+  padding: 30px 70px 40px 70px;
+`;
+
+export const Title = styled.h1``;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './componentes/App/App';
+import App from './componentsBusquedaVuelo/App/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
